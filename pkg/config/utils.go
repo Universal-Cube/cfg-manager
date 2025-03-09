139231509eb93292edcd8f09302c4fecf59b153b
Purpose <riyane.dev@gmail.com>
2025-03-09 23:05:54 +0100

    `feat(config): add utility functions for nested map access and key transformation`
    ---
    Added `getNestedMap` for traversing nested maps and `transformMapKeys` for consistent key transformation, enhancing map handling capabilities.

--- pkg/config/utils.go
+++ pkg/config/utils.go
@@ -48,3 +48,99 @@ func resolvePath(filePath string) (string, error) {
 
 	return filePath, nil
 }
+
+func getNestedMap(data map[string]interface{}, path string, caseSensitive bool) (map[string]interface{}, string, error) {
+	keys := strings.Split(path, ".")
+	if len(keys) == 0 {
+		return nil, "", errors.New("invalid path")
+	}
+
+	lastIndex := len(keys) - 1
+	lastKey := keys[lastIndex]
+	current := data
+
+	if lastIndex == 0 {
+		return current, lastKey, nil
+	}
+
+	for i := 0; i < lastIndex; i++ {
+		key := keys[i]
+
+		if !caseSensitive {
+			found := false
+			for k := range current {
+				if strings.EqualFold(k, key) {
+					key = k
+					found = true
+					break
+				}
+			}
+			if !found {
+				return nil, "", fmt.Errorf("key '%s' not found in path '%s'", key, path)
+			}
+		}
+
+		val, exists := current[key]
+		if !exists {
+			return nil, "", fmt.Errorf("key '%s' not found in path '%s'", key, path)
+		}
+
+		nestedMap, ok := val.(map[string]interface{})
+		if !ok {
+			iFaceMap, isIFaceMap := val.(map[interface{}]interface{})
+			if !isIFaceMap {
+				return nil, "", fmt.Errorf("key '%s' is not a map in path '%s'", key, path)
+			}
+
+			nestedMap = make(map[string]interface{})
+			for k, v := range iFaceMap {
+				strKey, ok := k.(string)
+				if !ok {
+					return nil, "", fmt.Errorf("key '%s' is not a string in path '%s'", key, path)
+				}
+				nestedMap[strKey] = v
+			}
+		}
+
+		current = nestedMap
+	}
+
+	if !caseSensitive {
+		for k := range current {
+			if strings.EqualFold(k, lastKey) {
+				lastKey = k
+				break
+			}
+		}
+	}
+
+	return nil, "", nil
+}
+
+func transformMapKeys(v interface{}) interface{} {
+	switch v := v.(type) {
+	case map[interface{}]interface{}:
+		m := make(map[string]interface{})
+		for k, val := range v {
+			strKey, ok := k.(string)
+			if !ok {
+				strKey = fmt.Sprintf("%v", k)
+			}
+			m[strKey] = transformMapKeys(val)
+		}
+		return m
+	case map[string]interface{}:
+		m := make(map[string]interface{})
+		for k, val := range v {
+			m[k] = transformMapKeys(val)
+		}
+		return m
+	case []interface{}:
+		for i, val := range v {
+			v[i] = transformMapKeys(val)
+		}
+		return v
+	}
+
+	return v
+}
